d784235a576919b5cb1a2413a9b41277c0549b59
Cody Mathisen <mizx@users.noreply.github.com>
2025-01-05 21:00:05 -0800

    fix device in darkice.cfg

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ bufferSecs      = ${BUFFER_SECS}
 reconnect       = yes
 
 [input]
-device          = default
+device          = stdin
 sampleRate      = ${SAMPLE_RATE_AUDIO}
 bitsPerSample   = ${BITS_PER_SAMPLE}
 channel         = ${CHANNEL}
@@ -38,7 +38,7 @@ EOF
 echo "Generated DarkIce configuration:"
 cat /etc/darkice.cfg
 
-# Run rtl_sdr, sox, and darkice
-rtl_sdr -d rtl_tcp=${RTL_TCP_SERVER}:1234 -g ${GAIN} -f ${FREQUENCY} -s ${SAMPLE_RATE} | \
+# Start RTL-SDR and pipe audio to sox and pipe again to DarkIce
+rtl_sdr -d rtl_tcp=${RTL_TCP_SERVER} -g ${GAIN} -f ${FREQUENCY} -s ${SAMPLE_RATE} | \
 sox -t raw -r ${SAMPLE_RATE} -e signed -b 16 -c 2 -V1 - -t wav - rate ${SAMPLE_RATE_AUDIO} | \
 darkice -c /etc/darkice.cfg
